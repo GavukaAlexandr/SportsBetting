be66047c2bc3dd2c42f81cf795a98d943db5a3f8
Alexandr Gavuka <alexandrgavuka@gmail.com>
2017-01-18 11:34:53 +0200

    .

--- src/Andersen/SportsBettingBundle/Command/CreateGamesCommand.php
+++ src/Andersen/SportsBettingBundle/Command/CreateGamesCommand.php
@@ -3,7 +3,6 @@
 namespace Andersen\SportsBettingBundle\Command;
 
 use Andersen\SportsBettingBundle\Entity\Game;
-use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -29,51 +28,54 @@ class CreateGamesCommand extends Command
 
         $this
             // the name of the command (the part after "bin/console")
-            ->setName('SportsBettingBundle:create-games')
+            ->setName('Sport:create-games')
             // the short description shown while running "php bin/console list"
             ->setDescription('Creates new games.')
             // the full command description shown when running the command with
             // the "--help" option
-            ->setHelp("This command allows you to create games...");
+            ->setHelp("This command allows you to create games...")
+            ->addArgument('number', InputArgument::REQUIRED, 'how many games do you want?')
+
+
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-//        $this->addArgument('number', InputArgument::REQUIRED, 'how many games do you want?');
-//        $number = $input->getArgument('number');
-//        $output->writeln("Yes my Lord, now will create $number games for you");
+        $number = $input->getArgument('number');
+        $output->writeln('Yes my Lord, now will create ' . $input->getArgument('number') . 'games for you');
 
-//        $sports = $this->em->getRepository('SportsBettingBundle:Sport')->findAll();
-////        $games = $this->em->getRepository('SportsBettingBundle:Game')->findAll();
-//
-//
-//
-////        запрос команд по видові спорту
-//
-//        foreach ($sports as $sport) {
-//
-//            $teams = $this->em->getRepository('SportsBettingBundle:Team')->findGamesOfSportType($sport->getId());
-//            $countTeams = count($teams) - 1;
-//
-//            for ($i = 1; $i < $number; $i++) {
-//
-//                $name1 = $teams[rand(1, $countTeams)];
-//                $name2 = $teams[rand(1, $countTeams)];
-//
-//                for ($i = $name1; $i == $name2;)
-//                {
-//                 $name2 = $teams[rand(1, $countTeams)];
-//                }
-//
-//                $name = $name1 . " " . $name2;
-//
-//                $gameType = new Game();
-//                $gameType->setName($name);
-//                $gameType->setSport($sport);
-//
-//                $this->em->persist($gameType);
-//                $this->em->flush();
-//            }
-//        }
+        $sports = $this->em->getRepository('SportsBettingBundle:Sport')->findAll();
+//        $games = $this->em->getRepository('SportsBettingBundle:Game')->findAll();
+
+
+
+//        запрос команд по видові спорту
+
+        foreach ($sports as $sport) {
+
+            $teams = $this->em->getRepository('SportsBettingBundle:Team')->findGamesOfSportType($sport->getId());
+            $countTeams = count($teams) - 1;
+
+            for ($i = 1; $i < $number; $i++) {
+
+                $name1 = $teams[rand(1, $countTeams)];
+                $name2 = $teams[rand(1, $countTeams)];
+
+                for ($i = $name1; $i == $name2;)
+                {
+                 $name2 = $teams[rand(1, $countTeams)];
+                }
+
+                $name = $name1 . " " . $name2;
+
+                $gameType = new Game();
+                $gameType->setName($name);
+                $gameType->setSport($sport);
+
+                $this->em->persist($gameType);
+                $this->em->flush();
+            }
+        }
     }
 }
